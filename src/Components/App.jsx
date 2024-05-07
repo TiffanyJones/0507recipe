702a685fe08cb3d7403c3723775ba4cf08ba9907
TiffanyJones <134651957+TiffanyJones@users.noreply.github.com>
2024-05-07 15:01:11 -0400

    initial commit

---
+++ src/Components/App.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import Recipe from "./Recipe";
+
+export default function App() {
+  const [categories, setCategories] = useState([]);
+  const [recipes, setRecipes] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("Seafood");
+
+  useEffect(() => {
+    fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
+      .then((response) => response.json())
+      .then((data) => setCategories(data.categories))
+      .catch((error) => console.error("Error:", error));
+  }, []);
+
+  useEffect(() => {
+    fetch(
+      "https://www.themealdb.com/api/json/v1/1/filter.php?c="+ selectedCategory
+    )
+      .then((response) => response.json())
+      .then((data) => setRecipes(data.meals))
+      .catch((error) => console.error("Error: ", error));
+  }, [selectedCategory]);
+
+const handleSelectedCategory = event => {
+  setSelectedCategory(event.target.value);
+}
+
+  return (
+    <>
+      <h1>My Favorite Recipes</h1>
+      <div className="categories">
+        <label htmlFor="categorySelect">Select Category: </label>
+        <select
+          id="categorySelect"
+          value={selectedCategory}
+          onChange={handleSelectedCategory}
+        >
+          {categories.map((category) => (
+            <option value={category.strCategory} key={category.idCategory}>
+              {category.strCategory}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="container">
+        {recipes.map((recipe) => (
+          <Recipe data={recipe} key={recipe.idMeal} />
+        ))}
+      </div>
+    </>
+  );
+}
